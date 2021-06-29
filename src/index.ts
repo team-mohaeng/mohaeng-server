e218e535ffdb5ddd2915e1a1b4b4e8463b88d4a7
Kim HyunJi <khyunjiee@gmail.com>
2021-06-29 11:06:16 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+const app = express();
+import connectDB from "./loader/db";
+
+// Connect Database
+connectDB();
+
+app.use(express.json());
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "production" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app
+  .listen(5000, () => {
+    console.log(`
+    ################################################
+    🛡️  Server listening on port: 5000 🛡️
+    ################################################
+  `);
+  })
+  .on("error", (err) => {
+    console.error(err);
+    process.exit(1);
+  });
