d6440fd78ae5310b1e50d48fcf797d0b5f6e1a98
Kim HyunJi <khyunjiee@gmail.com>
2021-06-30 17:27:17 +0900

    doc: 상수 파일 작성ㅇ

---
+++ src/constant.ts
@@ -0,0 +1,9 @@
+// 코스 진행 여부 (situation)
+const NOT_STATUS = 0;
+const IN_PROGRESS = 1;
+const COMPLETE = 2;
+
+// 에러 메세지
+const SERVER_ERROR_MESSAGE = "서버 에러입니다. 서버 파트에게 문의해주세요 *^^*"
+const TOKEN_ERROR_MESSAGE = "만료된 토큰입니다. 우리 아기 고앵이 토큰 하나 더 받아와 쪽-"
+const POST_ERROR_MESSAGE = "글을 찾을 수 없습니다. 쟈기 어떤 글을 보고싶은거야? 혹시.. 쟈니 글..?"
