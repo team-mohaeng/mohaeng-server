cbf2875022ae71867864cc0b687fd43da399c4dd
Kim HyunJi <khyunjiee@gmail.com>
2021-07-03 01:20:04 +0900

    feat: 로그인 서비스 작성

---
+++ src/service/userService.ts
@@ -0,0 +1,57 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "../config";
+
+import User from "../models/User";
+import { IFail } from "../interfaces/IFail";
+import UserSignInRequestDTO from "../dto/SignIn/UserSignInRequestDTO";
+import UserSignInResponseDTO from "../dto/SignIn/UserSignInResponseDTO";
+
+export default {
+  signin: async (dto: UserSignInRequestDTO) => {
+    try {
+      const { userId, userPw } = dto;
+
+      let user = await User.findOne({ userId });
+      if (!user) {
+        const notExistUser: IFail = {
+          status: 404,
+          message: "유저가 존재하지 않습니다.",
+        };
+        return notExistUser;
+      }
+
+      const isMatch = await bcrypt.compare(userPw, user.userPw);
+      if (!isMatch) {
+        const notMatchPw: IFail = {
+          status: 400,
+          message: "비밀번호가 일치하지 않습니다.",
+        };
+        return notMatchPw;
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      
+      const jwtToken = jwt.sign(
+        payload,
+        config.jwtSecret,
+        { expiresIn: 36000 },
+      );
+
+      const responseDTO: UserSignInResponseDTO = {
+        status: 200,
+        data: {
+          jwt: jwtToken,
+        },
+      };
+      
+      return responseDTO;
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+}
