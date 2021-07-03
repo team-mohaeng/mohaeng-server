e1b1e93bb38813e82d8506d59ddb905200f406d1
Kim HyunJi <khyunjiee@gmail.com>
2021-07-04 03:22:29 +0900

    feat: apidoc 환경설정

---
+++ apidoc/api_data.js
@@ -0,0 +1,68 @@
+define({ "api": [
+  {
+    "type": "get",
+    "url": "/user/:id",
+    "title": "Request User information",
+    "name": "GetUser",
+    "group": "User",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "id",
+            "description": "<p>User's unique ID.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "firstname",
+            "description": "<p>Firstname of the User.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "lastname",
+            "description": "<p>Lastname of the User.</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/api/apiDummy.ts",
+    "groupTitle": "User"
+  },
+  {
+    "type": "post",
+    "url": "/api/signup",
+    "title": "",
+    "version": "1.0.0",
+    "name": "SignUp",
+    "group": "User",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "jwt",
+            "description": ""
+          }
+        ]
+      }
+    },
+    "filename": "src/api/auth.ts",
+    "groupTitle": "User"
+  }
+] });
---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "쟈니",
+  "version": "1.0.0",
+  "description": "🌹 쟈기들과는 async 하고싶지 않아.. 오로지 sync.. 🌹",
+  "title": "쟈니",
+  "url": "https://api.github.com/v1",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-07-03T18:22:04.269Z",
+    "url": "https://apidocjs.com",
+    "version": "0.28.1"
+  }
+});
