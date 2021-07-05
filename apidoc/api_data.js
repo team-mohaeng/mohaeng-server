c4eae4d415f20f2464e085e0072e162c0f08e021
Kim HyunJi <khyunjiee@gmail.com>
2021-07-05 13:02:37 +0900

    feat: 코스 라이브러리 조회 api 구현

--- apidoc/api_data.js
+++ apidoc/api_data.js
@@ -1,6 +1,89 @@
 define({ "api": [
   {
     "type": "get",
+    "url": "/api/courses",
+    "title": "코스 라이브러리 조회 (코스 전체 조회)",
+    "version": "1.0.0",
+    "name": "GetCourses",
+    "group": "Course",
+    "header": {
+      "examples": [
+        {
+          "title": "Header-Example:",
+          "content": "{\n \"Content-Type\": \"application/json\",\n \"Authorization\": \"Bearer {jwt}\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "id",
+            "description": ""
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "situation",
+            "description": ""
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "title",
+            "description": ""
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "description",
+            "description": ""
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "totalDays",
+            "description": ""
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "property",
+            "description": ""
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "200 조회 성공\n{\n \"status\": 200,\n \"data\": {\n     \"course\": [\n         {\n             \"id\": 2,\n             \"situation\": 0,\n             \"title\": \"뽀득뽀득 세균퇴치\",\n             \"description\": \"나 쟈니가 인간세계에 처음 도착했을 때 사람들이 청결에 대해 은근히 무심한 것이 신기했쟈니. 내가 사는 별에서는 상상도 할 수 없쟈니.\",\n             \"totalDays\": 6,\n             \"property\": \"water\"\n         },\n       // ...\n     ]\n   }\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "403 만료된 토큰\n{\n \"status\": 403,\n \"message\": \"만료된 토큰입니다. 우리 아기 고앵이 토큰 하나 더 받아와 쪽-\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "src/api/course.ts",
+    "groupTitle": "Course"
+  },
+  {
+    "type": "get",
     "url": "/api/home",
     "title": "메인홈",
     "version": "1.0.0",
