48919c6ab88f1819710f5089f793eb4180f5e3c4
Kim HyunJi <khyunjiee@gmail.com>
2021-07-07 20:36:48 +0900

    feat: 현재 코스 전체 챌린지 조회 서비스 로직 구현

--- src/service/challengeService.ts
+++ src/service/challengeService.ts
@@ -4,6 +4,7 @@ import Course from "../models/Course";
 import TodayChallengeResponseDTO, { TodayChallengeDetailResponseDTO } from "../dto/Challenge/TodayChallenge/TodayChallengeResponseDTO";
 import StampChallengeResponseDTO, { StampChallengeDetailResponseDTO } from "../dto/Challenge/StampChallenge/StampChallengeResponseDTO";
 import { getDay, getMonth, getYear } from "../formatter/challengeDateFormatter";
+import GetChallengeResponseDTO, { ChallengeMapDetailResponseDTO } from "../dto/Challenge/GetChallenge/GetChallengeResponseDTO";
 
 export default {
   today: async (token: String, courseId: String, challengeId: String) => {
@@ -214,6 +215,81 @@ export default {
 
       // 최종 responseDTO
       const responseDTO: StampChallengeResponseDTO = {
+        status: 200,
+        data: {
+          course: {
+            id: userCourse.id,
+            situation: userCourse.situation,
+            title: dummyCourse.title,
+            description: dummyCourse.description,
+            totalDays: dummyCourse.totalDays,
+            property: dummyCourse.property,
+            challenges: challengeArray,
+          },
+        },
+      };
+      
+      return responseDTO;
+    } catch (err) {
+      console.error(err.message);
+    }
+  },
+  challenges: async (token: String) => {
+    try {
+      const user = await User.findOne({ id: token });
+      const courses = await Course.find();
+      let progressCourseId: number;
+      
+      // user jwt 토큰으로 유저 식별
+      if (!user) {
+        const notExistUser: IFail = {
+          status: 404,
+          message: "유저가 존재하지 않습니다.",
+        };
+        return notExistUser;
+      }
+      
+      // 진행 중인 코스 id 찾기
+      user.courses.forEach((course) => {
+        if (course.situation === 1) progressCourseId = course.id;
+      });
+      // 진행 중인 코스 id가 없는 경우
+      if (progressCourseId === null) {
+        const notExistsCourseId: IFail = {
+          status: 400,
+          message: "진행 중인 코스가 없습니다.",
+        };
+        return notExistsCourseId;
+      }
+      
+      const userCourse = user.courses[progressCourseId - 1];
+      const dummyCourse = courses[progressCourseId - 1];
+      const dummyChallenge = courses[progressCourseId - 1].challenges;
+
+      let challengeArray: Array<ChallengeMapDetailResponseDTO> = new Array<ChallengeMapDetailResponseDTO>();
+      userCourse.challenges.forEach((challenge) => {
+        let ments: Array<String> = new Array<String>();
+        dummyChallenge[challenge.id - 1].userMents.forEach((ment) => {
+          ments.push(ment.ment);
+        });
+
+        const responseChallenge: ChallengeMapDetailResponseDTO = {
+          id: challenge.id,
+          situation: challenge.situation,
+          title: dummyChallenge[challenge.id - 1].title,
+          description: dummyChallenge[challenge.id - 1].description,
+          year: challenge.year,
+          month: challenge.month,
+          day: challenge.day,
+          currentStamp: challenge.currentStamp,
+          totalStamp: dummyChallenge[challenge.id - 1].totalStamp,
+          userMents: ments
+        };
+        challengeArray.push(responseChallenge);
+      });
+      
+      // 최종 responseDTO
+      const responseDTO: GetChallengeResponseDTO = {
         status: 200,
         data: {
           course: {
