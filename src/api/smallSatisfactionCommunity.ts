84f6027639ba3af860248f71cae6b1008347b486
gamza55 <skkyuns@naver.com>
2021-07-09 01:17:55 +0900

    소확행 커뮤니티 apidoc 수정

--- src/api/smallSatisfactionCommunity.ts
+++ src/api/smallSatisfactionCommunity.ts
@@ -14,13 +14,20 @@ const router = express.Router();
  * @apiHeaderExample {json} Header-Example:
  * {
  *  "Content-Type": "application/json"
+ *  "Bearer": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiZXA0UmhZcmJUSE9uaHpBUldOVFNTMTpBUEE5MWJIS1pGdkJuUkV1dEEtYzQxSmN6dDBITzVJQkNyMFhzM0VadjFFcUZSVl9jY05semtDbFQtaWxmT3FGTUFWTmFPUFYxaVhIQjIybHhrcHZJRWNTNW4tMjQtZzY2SVR1d0o1aW9aWlJtYVd5R1Q3XzZiUDhlR1BOZHd2SkNwUWxZb1daQlhHVCJ9LCJpYXQiOjE2MjU3NjAxOTUsImV4cCI6MTYyNTc5NjE5NX0.jcxHZIN8cT268jCg98674B3rksW7gEb1tkNtfg1SfXU"
  * }
  * 
- * @apiParamExample {json} Request-Example:
- * {
- * }
  *
- * @apiSuccess {String} jwt
+ * @apiSuccess {Boolean} hasSmallSatisfaction
+ * @apiSuccess {Number} userCount
+ * @apiSuccess {Array} [smallSatisfactions]
+ * @apiSuccess {Number} postId
+ * @apiSuccess {String} moodImage
+ * @apiSuccess {Array} [hashtags]
+ * @apiSuccess {String} content
+ * @apiSuccess {Number} likeCount
+ * @apiSuccess {Boolean} hasLike
+ * @apiSuccess {String} nickname
  * 
  * @apiSuccessExample {json} Success-Response:
  * 200 OK
@@ -36,7 +43,6 @@ const router = express.Router();
  *				"hashtags": ["#해쉬태그1", "#해쉬태그2", ... ],
  *				"content": "맛있는 피자에 시원한 맥주 ... ",
  *				"likeCount": 72,
- *				"hasImage": false,
  *				"hasLike": true,
  *				"nickname": "시원스쿨"
  *			},
@@ -46,7 +52,6 @@ const router = express.Router();
  *				"hashtags": ["#해쉬태그1", "#해쉬태그2", ... ],
  *				"content": "맛있는 피자에 시원한 맥주 ... ",
  *				"likeCount": 72,
- *				"hasImage": false,
  *				"hasLike": true,
  *				"nickname": "시원스쿨"
  *			},
