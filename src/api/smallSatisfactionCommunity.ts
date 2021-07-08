dadecac16294c461724d8010c906814515f1d19b
gamza55 <skkyuns@naver.com>
2021-07-09 01:59:36 +0900

    소확행 커뮤니티 조회 api http 요청 방식, HeaderExample 수정

--- src/api/smallSatisfactionCommunity.ts
+++ src/api/smallSatisfactionCommunity.ts
@@ -5,7 +5,7 @@ import smallSatisfactionCommunityService from "../service/smallSatisfactionCommu
 const router = express.Router();
 
 /**
- * @api {post} /api/smallSatisfactionCommunity 커뮤니티 소확행 글 조회
+ * @api {get} /api/smallSatisfactionCommunity 커뮤니티 소확행 글 조회
  * 
  * @apiVersion 1.0.0
  * @apiName smallSatisfactionCommunity
@@ -14,7 +14,7 @@ const router = express.Router();
  * @apiHeaderExample {json} Header-Example:
  * {
  *  "Content-Type": "application/json"
- *  "Bearer": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiZXA0UmhZcmJUSE9uaHpBUldOVFNTMTpBUEE5MWJIS1pGdkJuUkV1dEEtYzQxSmN6dDBITzVJQkNyMFhzM0VadjFFcUZSVl9jY05semtDbFQtaWxmT3FGTUFWTmFPUFYxaVhIQjIybHhrcHZJRWNTNW4tMjQtZzY2SVR1d0o1aW9aWlJtYVd5R1Q3XzZiUDhlR1BOZHd2SkNwUWxZb1daQlhHVCJ9LCJpYXQiOjE2MjU3NjAxOTUsImV4cCI6MTYyNTc5NjE5NX0.jcxHZIN8cT268jCg98674B3rksW7gEb1tkNtfg1SfXU"
+ *  "Bearer": "jwt"
  * }
  * 
  *
