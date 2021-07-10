4f92e4dc1125e65f91846a018cf33fe7ab305688
gamza55 <skkyuns@naver.com>
2021-07-11 06:20:11 +0900

    feat: 소확행 서비스 로직 병합

--- src/service/smallSatisfactionService.ts
+++ src/service/smallSatisfactionService.ts
@@ -2,8 +2,9 @@ import SmallSatisfaction from "../models/SmallSatisfaction"
 import User from "../models/User";
 import { SmallSatisfactionWriteRequestDTO } from "../dto/SmallSatisfaction/Write/request/SmallSatisfactionWriteDTO"
 import { SmallSatisfactionWriteResponseDTO } from "../dto/SmallSatisfaction/Write/response/SmallSatisfactionWriteDTO"
-import SmallSatisfactionMyDrawerResponseDTO, { SmallSatisfactionResponseDTO } from "../dto/SmallSatisfaction/MyDrawer/response/SmallSatisfactionMyDrawerResponseDTO";
+import SmallSatisfactionMyDrawerResponseDTO, { myDrawerResponseDTO } from "../dto/SmallSatisfaction/MyDrawer/response/SmallSatisfactionMyDrawerResponseDTO";
 import SmallSatisfactionCommunityResponseDTO, { CommunityResponseDTO } from "../dto/SmallSatisfaction/Community/response/SmallSatisfactionCommunityResponseDTO";
+import SmallSatisfactionDetailResponseDTO from "../dto/SmallSatisfaction/Detail/response/SmallSatisfactionDetailResponseDTO";
 import { IFail } from "../interfaces/IFail";
 
 export default {
@@ -63,8 +64,8 @@ export default {
       console.error(err.message);
     }
   },
-  /*
-  myDrawer: async (token: String, year: String, month: String) => {
+  
+  myDrawer: async (token: String, year: string, month: string) => {
     const user = await User.findOne({ id: token });
     if (!user) {
       const notExistUser: IFail = {
@@ -74,51 +75,52 @@ export default {
       return notExistUser;
     }
 
-    try{  
-      let myDrawerSmallSatisfactions = await SmallSatisfaction.find({ user: user._id, year: year, month: month }).sort({ date: -1 });
-  
-      let responseSmallSatisfaction: Array<SmallSatisfactionResponseDTO> = new Array<SmallSatisfactionResponseDTO>();
+    //SmallSatisfactionMyDrawerResponseDTO, myDrawerResponseDTO
+
+    let myDrawerSmallSatisfactions = await SmallSatisfaction.find({ user: user._id, year: year, month: month }, {}, {}).sort({ date: -1 });
+    try{
+      let myDrawers: Array<myDrawerResponseDTO> =  new Array<myDrawerResponseDTO>();
       myDrawerSmallSatisfactions.forEach((myDrawerSmallSatisfaction) => {
         let liked;
         if (myDrawerSmallSatisfaction.likes.filter((like) => like.user.toString() === token)
             .length > 0
-        ) {
-          liked = true;
-        }
-        else {
-          liked = false;
-        }
+      ) {
+        liked = true;
+      }
+      else {
+        liked = false;
+      }
 
-        const responseDTO: SmallSatisfactionResponseDTO = {
-          postId: myDrawerSmallSatisfaction.postId,
-          nickname: user.nickname,
-          moodImage: myDrawerSmallSatisfaction.moodImage,
-          mainImage: myDrawerSmallSatisfaction.mainImage,
-          likeCount: myDrawerSmallSatisfaction.likes.length,
-          content: myDrawerSmallSatisfaction.content,
-          hasLike: liked,
-          hashtags: myDrawerSmallSatisfaction.hashtags,
-          year: myDrawerSmallSatisfaction.year,
-          month: myDrawerSmallSatisfaction.month,
-          day: myDrawerSmallSatisfaction.day,
-          date: myDrawerSmallSatisfaction.date,
-        }
-        responseSmallSatisfaction.push(responseDTO);
-      });
+
+      const responseDTO: myDrawerResponseDTO = {
+        postId: myDrawerSmallSatisfaction.postId,
+        nickname: user.nickname,
+        moodImage: myDrawerSmallSatisfaction.moodImage,
+        mainImage: myDrawerSmallSatisfaction.mainImage,
+        likeCount: myDrawerSmallSatisfaction.likes.length,
+        content: myDrawerSmallSatisfaction.content,
+        hasLike: liked,
+        hashtags: myDrawerSmallSatisfaction.hashtags,
+        year: myDrawerSmallSatisfaction.year,
+        month: myDrawerSmallSatisfaction.month,
+        day: myDrawerSmallSatisfaction.day,
+        date: myDrawerSmallSatisfaction.date,
+      }
+      myDrawers.push(responseDTO);
+    });
+
+    const responseDTO: SmallSatisfactionMyDrawerResponseDTO = {
+      status: 200,
+      data: {
+        myDrawerSmallSatisfactions: myDrawers
+      }
+    };
+    return responseDTO;
+  } catch (err) {
+    console.error(err);
+  }
+},
   
-      const responseDTO: SmallSatisfactionMyDrawerResponseDTO = {
-        status: 200,
-        data: {
-          myDrawerSmallSatisfaction: responseSmallSatisfaction,
-        }
-      };
-      return responseDTO; 
-      
-    } catch (err) {
-      console.error(err.message);
-    }
-  },
-  */
   community: async (token: String, sort: String) => {
     const user = await User.findOne({ id: token });
     if (!user) {
@@ -161,6 +163,9 @@ export default {
     try{  
       let responseSmallSatisfaction: Array<CommunityResponseDTO> = new Array<CommunityResponseDTO>();
       communitySmallSatisfactions.forEach((communitySmallSatisfaction) => {
+        
+        let userNickname = "투명견";
+
         let liked;
         if (communitySmallSatisfaction.likes.filter((like) => like.user.toString() === token)
             .length > 0
@@ -173,7 +178,7 @@ export default {
 
         const responseDTO: CommunityResponseDTO = {
           postId: communitySmallSatisfaction.postId,
-          nickname: user.nickname,
+          nickname: userNickname,
           moodImage: communitySmallSatisfaction.moodImage,
           mainImage: communitySmallSatisfaction.mainImage,
           likeCount: communitySmallSatisfaction.likes.length,
@@ -197,9 +202,66 @@ export default {
         }
       };
       return responseDTO;
-
     } catch (err) {
       console.error(err.message);
+    }
+  },
+  detail: async (token: String, postId: string) => {
+    let postNumber = parseInt(postId);
+    const user = await User.findOne({ id: token });
+    if (!user) {
+      const notExistUser: IFail = {
+        status: 400,
+        message: "유저가 존재하지 않습니다.",
+      };
+      return notExistUser;
+    }
+
+    try {
+      let detailSmallSatisfaction = await SmallSatisfaction.findOne({ postId: postNumber });
+      if (!detailSmallSatisfaction) {
+          const notExistSatisfaction: IFail = {
+            status: 400,
+            message: "소확행이 존재하지 않습니다.",
+          };
+          return notExistSatisfaction;
+      }
+
+
+      let liked;
+      if (detailSmallSatisfaction.likes.filter((like) => like.user.toString() === token)
+          .length > 0
+      ) {
+        liked = true;
+      }
+      else {
+        liked = false;
+      }
+  
+      let smallSatisfactionUser = await User.findOne({ _id: detailSmallSatisfaction.user });
+      let userNickname = smallSatisfactionUser.nickname;
+    
+      const responseDTO: SmallSatisfactionDetailResponseDTO = {
+        status: 200,
+        data: {
+          postId: detailSmallSatisfaction.postId,
+          nickname: userNickname,
+          moodImage: detailSmallSatisfaction.moodImage,
+          mainImage: detailSmallSatisfaction.mainImage,
+          likeCount: detailSmallSatisfaction.likes.length,
+          content: detailSmallSatisfaction.content,
+          hasLike: liked,
+          hashtags: detailSmallSatisfaction.hashtags,
+          year: detailSmallSatisfaction.year,
+          month: detailSmallSatisfaction.month,
+          day: detailSmallSatisfaction.day,
+          date: detailSmallSatisfaction.date
+        }
+      }
+  
+      return responseDTO; 
+    } catch (err) {
+    console.error(err.message);
     }
   }
 }
