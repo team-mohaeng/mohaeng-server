1e8a94ca8222b26bffaf9d440345f1b17cea572b
gamza55 <skkyuns@naver.com>
2021-07-11 06:19:43 +0900

    feat: 소확행 내서랍장 response DTO 수정

--- src/dto/SmallSatisfaction/MyDrawer/response/SmallSatisfactionMyDrawerResponseDTO.ts
+++ src/dto/SmallSatisfaction/MyDrawer/response/SmallSatisfactionMyDrawerResponseDTO.ts
@@ -4,11 +4,11 @@ import { IHashtag } from "../../../../interfaces/IHashtag";
 export default interface SmallSatisfactionMyDrawerResponseDTO {
 	status: number;
 	data: {
-		myDrawerSmallSatisfaction: SmallSatisfactionResponseDTO[];
+		myDrawerSmallSatisfactions: myDrawerResponseDTO[];
 	}
 }
 
-export interface SmallSatisfactionResponseDTO {
+export interface myDrawerResponseDTO {
 	postId: number;
 	nickname: string;
 	moodImage: IImage;
