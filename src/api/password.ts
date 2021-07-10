a0ad8f29b5d2c969d38c96ff8610fce9b39ef28f
Kim HyunJi <khyunjiee@gmail.com>
2021-07-11 02:20:53 +0900

    feat: 비밀번호 변경 api 구현

--- src/api/password.ts
+++ src/api/password.ts
@@ -1,4 +1,7 @@
 import express, { Router } from "express";
+import { check, validationResult } from "express-validator";
+import password from "../controller/password";
+import ChangePasswordRequestDTO from "../dto/Password/ChangePasswordRequestDTO";
 import passwordService from "../service/passwordService";
 
 const router = express.Router();
@@ -8,5 +11,31 @@ router.get("/userId/:userId", async (req, res) => {
 
   res.json(result);
 });
+
+router.put(
+  "/", 
+  [
+    check("userId", "이메일 형태가 아닙니다.").isEmail(),
+    check("userPw", "비밀번호는 영문과 숫자를 포함한 8~16자로 입력해주세요.").isAlphanumeric().isLength({ min: 8, max: 16 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ 
+        status: 400,
+        errors: errors.array()
+      });
+    }
+
+    const requestDTO: ChangePasswordRequestDTO = {
+      userId: req.body.userId,
+      userPw: req.body.userPw
+    };
+
+    const result = await passwordService.change(requestDTO);
+
+    res.json(result);
+  }
+  )
 
 module.exports = router;
